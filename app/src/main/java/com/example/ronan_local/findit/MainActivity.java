28a8a797eb6353c3a837e5376f7ea39e9791f7fd
Ronan Craig <ronan.craig01@googlemail.com>
2017-05-08 09:27:35 +0100

    Added Cinima Button and Map activity

--- app/src/main/java/com/example/ronan_local/findit/MainActivity.java
+++ app/src/main/java/com/example/ronan_local/findit/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ronan_local.findit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v7.app.ActionBar.LayoutParams;
+import android.view.LayoutInflater;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
+
+        LayoutParams lp1 = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        View customNav = LayoutInflater.from(this).inflate(R.layout.action_bar, null);
+        getSupportActionBar().setCustomView(customNav, lp1);
+    }
+
+    public void showCinimas(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
     }
 }
