19d9790b1ebf12c4a63e10e6c5525d174ff2749d
Ronan Craig <ronan.craig01@googlemail.com>
2017-05-11 18:53:53 +0100

    Hid action bar

--- app/src/main/java/com/example/ronan_local/findit/MainActivity.java
+++ app/src/main/java/com/example/ronan_local/findit/MainActivity.java
@@ -14,11 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-
-        LayoutParams lp1 = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        View customNav = LayoutInflater.from(this).inflate(R.layout.action_bar, null);
-        getSupportActionBar().setCustomView(customNav, lp1);
+        getSupportActionBar().hide();
     }
 
     public void showMap(View view)
