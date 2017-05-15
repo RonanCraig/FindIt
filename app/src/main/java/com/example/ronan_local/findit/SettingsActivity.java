0751047d94305a2460cb61d15b3794687e190f0b
Ronan Craig <ronan.craig01@googlemail.com>
2017-05-15 11:20:37 +0100

    on click method for settings exit button

--- app/src/main/java/com/example/ronan_local/findit/SettingsActivity.java
+++ app/src/main/java/com/example/ronan_local/findit/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.ronan_local.findit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void buttonExitClicked(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
