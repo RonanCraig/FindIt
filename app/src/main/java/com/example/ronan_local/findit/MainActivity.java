81840f35f35cd3fb37dc4bbca0e412b0d6e802cb
Chris <c.abel-15@student.lboro.ac.uk>
2017-05-17 18:50:32 +0100

    Checks if location is enabled on the phone Map Activity now shows icon when opened

--- app/src/main/java/com/example/ronan_local/findit/MainActivity.java
+++ app/src/main/java/com/example/ronan_local/findit/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.ronan_local.findit;
 
 import android.Manifest;
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,25 +32,53 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    protected LocationManager location;
+    boolean gps = false;
+    boolean network = false;
+
     public void showMap(View view)
     {
-        Intent intent = new Intent(this, MapsActivity.class);
-        if(view.getId() == R.id.cinema_btn)
-        {
-            intent.putExtra("typesToSearch","movie_theater");
+        //Finds out if device location is turned on
+        location = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            gps = location.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {}
+        try{
+            network = location.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {}
+
+        //Checks if location turned on, if not it requests that the user turns it on manually
+        if(!gps && !network){
+            AlertDialog locationOff = new AlertDialog.Builder(this).create();
+            locationOff.setTitle("Location is disabled");
+            locationOff.setMessage("Please turn on location in settings");
+            locationOff.setButton(AlertDialog.BUTTON_NEUTRAL, "Okay",
+                    new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            locationOff.show();
         }
-        else if(view.getId() == R.id.restaurant_btn)
-        {
-            intent.putExtra("typesToSearch","restaurant|cafe|meal_takeaway");
+
+        //Checks if it has location permissions, if not asks for permission
+        else if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-        else if(view.getId() == R.id.carpark_btn)
-        {
-            intent.putExtra("typesToSearch","parking");
+
+        else {
+            Intent intent = new Intent(this, MapsActivity.class);
+            if (view.getId() == R.id.cinema_btn) {
+                intent.putExtra("typesToSearch", "movie_theater");
+            } else if (view.getId() == R.id.restaurant_btn) {
+                intent.putExtra("typesToSearch", "restaurant|cafe|meal_takeaway");
+            } else if (view.getId() == R.id.carpark_btn) {
+                intent.putExtra("typesToSearch", "parking");
+            } else if (view.getId() == R.id.park_btn) {
+                intent.putExtra("typesToSearch", "park");
+            }
+            startActivity(intent);
         }
-        else if(view.getId() == R.id.park_btn)
-        {
-            intent.putExtra("typesToSearch","park");
-        }
-        startActivity(intent);
+
     }
 }
