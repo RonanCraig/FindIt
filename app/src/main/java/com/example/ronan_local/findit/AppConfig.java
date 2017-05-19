0fb5959213c5f1d6143a88ff99cd4157c7472074
Timtti <tmoodey@gmail.com>
2017-05-19 01:44:17 +0100

    app config file changed

--- app/src/main/java/com/example/ronan_local/findit/AppConfig.java
+++ app/src/main/java/com/example/ronan_local/findit/AppConfig.java
@@ -4,7 +4,7 @@ public class AppConfig {
     // Server user login url
     public static String URL_LOGIN = "http://cotm.sci-project.lboro.ac.uk/android_login_api/login.php";
     public static String URL_FRIENDS = "http://cotm.sci-project.lboro.ac.uk/android_login_api/friends.php";
-
+    public static String URL_REQUESTS = "http://cotm.sci-project.lboro.ac.uk/android_login_api/requests.php";
     // Server user register url
     public static String URL_REGISTER = "http://cotm.sci-project.lboro.ac.uk/android_login_api/register.php";
 }
